542268797b4e61f3347be82a795ab2686204db10
o-louis <oriane.louis.pro@gmail.com>
2021-05-23 19:19:12 -0400

    feat(fastify): Create authenticate plugin using jwtVerify

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
+import autoLoad from "fastify-autoload";
 import cors from "fastify-cors";
 import jwt from "fastify-jwt";
 import postgres from "fastify-postgres";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = async (fastify, opts) => {
   fastify.register(jwt, { secret: process.env.JWT_SECRET });
   fastify.register(cors, { origin: process.env.CORS_ORIGIN });
   fastify.register(postgres, {
     connectionString: process.env.DB_CONNECTION_STRING,
+  });
+
+  fastify.register(autoLoad, {
+    dir: join(__dirname, "plugins"),
+    options: { ...opts },
   });
 };
 
